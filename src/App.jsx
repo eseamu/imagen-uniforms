b3bc82e24299c79715f3719beb6f4cc67c75adc4
Esequiel Amuchastegui <esequielamuchastegui@MacBook-Air-de-Esequiel.local>
2021-06-05 19:57:37 -0300

    Material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div className="App">
       <h1>Bienvenidos a Imagen Uniformes!</h1>
+      <Button>CLICK PARA INGRESAR</Button>
     </div>
   );
 }
